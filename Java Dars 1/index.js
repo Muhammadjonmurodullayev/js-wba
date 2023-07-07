c74a51f5083f00a8d9abda010e3d92e2d884d172
Muhammadjon <muhammadjonmurodullayev@gmail.com>
2023-07-07 17:06:33 +0500

    Dasr-1

--- Java Dars 1/index.js
+++ Java Dars 1/index.js
@@ -1,2 +1,30 @@
-//                                  git hup
-console.log("name");
+// let 
+// var
+// const
+{
+    let age1=19
+    console.log(age1);
+    var age2=19
+
+}
+console.log(age2);
+
+
+let name = "webbrain"
+var surname= "academiy"
+const age = 18
+// console.log(name,surname,age);
+
+
+
+
+
+//                                    Data typelar 
+// pirinitiv,
+// Number
+// console.log(1);
+
+
+
+//  none pirimitiv
+// console.log("web");
