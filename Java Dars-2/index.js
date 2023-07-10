6a5ac2832b559b4a763a5e7bb466bce2d3e807b7
Muhammadjon <muhammadjonmurodullayev@gmail.com>
2023-07-10 18:30:08 +0500

    Dasr-2

---
+++ Java Dars-2/index.js
@@ -0,0 +1,62 @@
+// let num = "11.23"
+// console.log(num);
+// console.log(+num);
+// console.log(Number(num));
+// console.log(Number.parseInt(num));
+// console.log();
+// let obj= {
+//     name:"Muhammadjon",
+//     surname:"Murodullayev",
+//     age:18
+
+// }
+// let son = Object.keys(obj)
+// console.log(son);
+
+
+// const btn = document.getElementById('btn');
+
+// btn.addEventListener('click', () => {
+//   const form = document.getElementById('form');
+
+//   if (form.style.display === 'none') {
+//     //  this SHOWS the form
+//     form.style.display = 'block';
+//   } else {
+//     //  this HIDES the form
+//     form.style.display = 'none';
+//   }
+// });
+// const btn = document.getElementById('btn');
+
+// btn.addEventListener('click', () => {
+
+//   if (form.style.display === 'none') {
+//     //  this SHOWS the form
+//     form.style.display = 'block';
+//   } else {
+//     //  this HIDES the form
+//     form.style.display = 'none';
+//   }
+// // });
+// const form = document.getElementById('s');
+// function getabs() {
+
+// }
+// getabs()let 
+Number
+let num = "123"
+console.log(Number(num));
+console.log(num);
+console.log(Number.parseInt(num));
+console.log(Number.parseFloat(num));
+// Homework
+let value = true
+console.log(typeof value);// Boolean
+value=String(value)
+console.log(typeof value);// String
+let str = "123"
+console.log(typeof str);// String
+let num1= Number(str)
+console.log(typeof num1);// Number 
+
