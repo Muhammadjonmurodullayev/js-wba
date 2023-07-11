b5c7418dcf1115353d6053f408334bc1f56ae7f7
Muhammadjon <muhammadjonmurodullayev@gmail.com>
2023-07-11 15:42:03 +0500

    Dars-2

--- Java Dars-2/index.js
+++ Java Dars-2/index.js
@@ -64,4 +64,39 @@ let son = Number("Lorem")
 console.log(son);
 NaN //--Is NaN yani raqm bo'lmagan malumot turi!1
 // alert(son)
-
+let num2=  5>4
+console.log(num2);
+let syt="apple"
+let syt1="pineapple"
+console.log(syt>syt1);
+let num3=  "2">"12"
+console.log(num3);
+let syt3=undefined==null
+console.log(syt3);
+let syt4=undefined==null
+console.log(syt4);
+let syt5=null=="\n0\n"
+console.log(syt5);
+let syt6=null=== +"\n0\n"
+console.log(syt6);
+let a = 1,b=1
+let c = ++a; 
+let d=b++;
+console.log(a,b,c,d);
+// a=2 b=2 c=2 d=1
+let a1= 2 
+let x=1 +(a*=2)//x=5
+console.log(x);
+let q =""+ 1+0 //10 string 
+let w =""+ 1+0//10 string
+let e =true+ false//1
+let r =6/"3"// 2
+let t="2"*"3"//6
+let y=4+5+"$"//9$
+let u=4-"2"// 2
+let i ="4px"-2//NaN
+let o ="-9"-5//-14
+let p = null+1//1
+let s= undefined+1//NaN
+let g="\t \n"-2// -2
+console.log(q,w,e,r,t,y,u,i,o,p,s,g);
