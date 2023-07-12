14ca0c5941619d7b15118f8804150b7e6f591c34
Muhammadjon <muhammadjonmurodullayev@gmail.com>
2023-07-12 17:35:19 +0500

    Dars-3

--- Java Dars-3/index.js
+++ Java Dars-3/index.js
@@ -1,2 +1,72 @@
-console.log(undefined ?? false);
-console.log("salom" ?? 78);
+// console.log(undefined ?? false);
+// console.log("salom" ?? 78);
+// 1)Misol
+// ==========================================================================================================================================
+// let son = 12
+// if (son > 0) {
+//     console.log(++son);
+// } else {
+//     console.log(son);
+// }
+// ==========================================================================================================================================
+// 2) misol
+// let son1 = 12
+// if (son1 > 0) {
+//     console.log(++son1);
+// } else {
+//     console.log(--son1);
+// }
+// =======================================================================================================================================
+// 3)misol
+// let son2 = 0
+// if (son2 > 0) {
+//     console.log(++son2);
+// }
+// else if (son2 < 0) {
+//     console.log(--son2);
+// } else if (son2 == 0) {
+//     console.log(son = 10);
+// }
+// =======================================================================================================================================
+// 3)misol
+// function get(a){
+// for( i of  a ){
+//     console.log(i);
+// }
+// }
+// get(1)
+// let a= `10123`
+// for ( i of a){
+//     if(i>0){
+//      console.log(`${+i} Musbat son`);
+//     }else if (i==0){
+//         console.log(`${i} Juft son `);
+//     }
+//     else{
+//         console.log(`${i} Manfiy san`);
+//     }
+// }
+// 4) Misol
+// let son = -1
+// let str = -2
+// let num = 5
+// if (son>0) {
+//     console.log(`${son} Musbat son`);
+//     if (str>0) {
+//         console.log(`${str} Musbat son`)
+//     }else{
+//         console.log(`${str} Manfiy son`);
+//     }
+// }else{
+//     console.log(`${son} Manfiy son`);
+// }
+let a=3
+let b=3
+if(a>b){
+    console.log(`${a} Kichik`);
+}
+else if(a<b){
+    console.log(`${b} Katta`);
+}else{
+    console.log("Sonlar bir biriga teng");
+}
