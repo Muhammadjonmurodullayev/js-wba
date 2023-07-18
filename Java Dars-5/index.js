b4a80ce443cc068e2d9af59153c1c3e25ffc9ebc
Muhammadjon <muhammadjonmurodullayev@gmail.com>
2023-07-18 19:05:25 +0500

    Dars-5

---
+++ Java Dars-5/index.js
@@ -0,0 +1,27 @@
+// function getname(n) {
+//     for (let n = 1234; n > n.length; n--) {
+//         console.log(n);
+//     }
+// }
+// getname(1234)
+// let name = "1234567"
+// console.log();
+// for (let n = "1213"; n < n.length(); n--) {
+//     // console.log(n);
+// }
+// let i = `1234`
+// console.log(i.length);
+
+// let a = "Muha"
+// let b = ""
+// for (let i = a.length - 1; i >= 0; i--) {
+//     b += a[i]
+// }
+// console.log(b);
+
+let a = "Muhammadjon";
+let b = "";
+for (let i = a.length - 1; i >= 0; i--) {
+    b += a[+i]
+}
+console.log(b);
