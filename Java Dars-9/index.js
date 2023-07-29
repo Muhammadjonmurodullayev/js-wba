1a083f2075468673b32ebd74362ceb9cf5d62e96
Muhammadjon <muhammadjonmurodullayev@gmail.com>
2023-07-29 13:59:27 +0500

    Dars-9

--- Java Dars-9/index.js
+++ Java Dars-9/index.js
@@ -37,11 +37,14 @@ let son4 = son3.toString(2); // 10 => 2 o'nlikdan ikkilika o'tish
 //  ==, === Object.is()
 // console.log(Object.is(23, "12")); //false
 // console.log(Object.is(23, 23)); //true
+// ====================================================
 //!   Math PI
 // console.log(Math.PI); 3.141592653589793 yani P qiymat 3.14 ga teng
+// =====================================================
 //!   Math abs Modul 
 // console.log(Math.abs(-2));// 2
 // console.log(Math.abs(0)); // 
+// ======================================================
 // pow(son,daraja) darsjani chiqarib berdi 
 // console.log(Math.pow(0, 4)); // 16
 //! sqrt()
@@ -62,9 +65,13 @@ let son4 = son3.toString(2); // 10 => 2 o'nlikdan ikkilika o'tish
 // floor  pastga qarab yaxlitlaydi;
 // console.log(Math.floor(2.99));
 // ============================================================
-// let son5 = Math.trunc(12.6);
-// let son8 = Math.floor(12.6);
-// let son6 = Math.round(12.5);
-// let son7 = Math.ceil(12.2);
-
-// console.log(son5, son8);
+let son5 = Math.trunc(12.6);
+let son8 = Math.floor(12.6);
+let son6 = Math.round(12.5);
+let son7 = Math.ceil(12.2);
+console.log(son5, son6, son8, son7);
+// ============================================================
+let sum = Math.sign(10) // 1
+let sum9 = Math.sign(-10) //-1
+let sum0 = Math.sign(0) // 0
+    // console.log(sum);
