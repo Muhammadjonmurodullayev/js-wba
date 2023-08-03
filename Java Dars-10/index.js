6c652b75d89e146809f8ab347c10db89de56b036
Muhammadjon.2005 <Muhammadjonmurodullayev2005@gmail.com>
2023-08-03 15:02:44 +0500

    Dars-10

--- Java Dars-10/index.js
+++ Java Dars-10/index.js
@@ -1,1 +1,22 @@
-console.log("String");
+// console.log("String");
+// console.log(str[1]);
+// console.log(str[-1]);////Index misus qiymat olmaydi 
+// console.log(str.at(-1));Minuslik qiymat qabul qiladi 
+// console.log(str.charAt(1));////Bu minuslik qiymat qabul qilmaydi
+// console.log(str.length);
+// console.log(str.includes("in")) ///// bu bizga ichida bor yoki yo'qligini tekshirib beradi bu bizga ture yoki flase qaytaradi
+// console.log(str.padStart(2, "."));
+// console.log(str.padEnd(4, "."));
+// console.log(str.startsWith(`Web`)); //Boshida shu malumot bor yoki yo'qligini tekshirib beradi 
+// console.log(str.endsWith(`miy`)); //oxirida shu so'z bor yoki yo'qligini teshirib beradi
+// console.log(str.substr(0, 5));
+// console.log(eval("2+2*2"));
+// let str2 = "dasturlash "
+// console.log(str.concat(str2))
+// console.log(str.trim()); // Bu boshidagi va oxiridagi bo'sh joylarni qirqib tashlaydi
+// console.log(new String("web").valueOf()); // constractorni malumotini olish
+// console.log(str.replace("web, Web"));
+let str = "Webbarain academiy"
+for (i of str) {
+    console.log(i);
+}
