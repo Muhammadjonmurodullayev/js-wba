34ac39bb73b0b4af0385790aa2e151406d7aa803
Muhammadjon.2005 <Muhammadjonmurodullayev2005@gmail.com>
2023-08-08 10:23:29 +0500

    Dars-12

--- Java Dars-11/index.js
+++ Java Dars-11/index.js
@@ -1,1 +1,14 @@
-console.log("err");
+// console.log("err");
+// console.log([1, true, false, {}, Symbol, Number, NaN]);
+// let arr1 = new Array(5)
+// let arr2 = new Array("5")
+// console.log(arr1.length, arr2);
+// let str = [1, 2, 3, 4, ]
+// let str1 = [4, 5, 6, 7, ]
+// console.log(str.concat(str1));
+let arr = ["webbrain", "Academy", ]
+    // arr.unshift("web") //Boshidan malumot qo'shadi
+    // arr.pop("web") //Oxiridan  malumot qo'shadi
+    // arr.shift() //Arrni Boshidan malumot o'chiradi
+    // arr.push() //Arr ni oxiridan Malumot o'chiradi
+    // console.log(arr);
