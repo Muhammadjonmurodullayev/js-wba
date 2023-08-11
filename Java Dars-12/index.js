7aecc61c3604e7a3d833dda1478b3adfb652b1bf
Muhammadjon.2005 <Muhammadjonmurodullayev2005@gmail.com>
2023-08-11 20:42:22 +0500

    Dars-12

--- Java Dars-12/index.js
+++ Java Dars-12/index.js
@@ -25,60 +25,104 @@
 //     console.log(v);
 // })
 // console.log(res);
+// const getbayid = () => { ////   2000 chi yildan oldin chiqqan mashinalar ro'yxait
+//         let res = user.filter((v) => {
+//             return v.year < 2000
+//         })
+//         user = res
+//         console.log(res);
+//     }
+//     // getbayid()
+// const getbay = () => {
+//         let res1 = user.filter((v) => {
+//             return v.year > 2010
+//         })
+//         user = res1
+//         console.log(res1);
+//     }
+//     // getbay()
+// const getbays = () => {
+//         let res2 = user.sort((a, b) => {
+//             return a.engine - b.engine
+//         })
+//         user = res2
+//         console.log(res2);
+//     }
+//     // getbays()
+// const getbayd = () => {
+//         let res4 = user.sort((a, b) => {
+//             return a.year - b.year
+//         })
+//         user = res4
+//         console.log(res4);
+//     }
+//     // getbayd()
+// const getbayf = () => {
+//         let res5 = user.sort((a, b) => {
+//             return a.name.localeCompare(b.name)
+//         })
+//         user = res5
+//         console.log(res5);
+//     }
+//     // getbayf()
+
+// let res = user.filter((valyus, index, Arr) => {//biz bergan qiymatni chiqaradi 
+//         // return index
+//     })
+//     // console.log(res);
+// let user1 = user.sort((a, b) => {
+//     return a.engine - b.engine
+// })
+// console.log(user1);
+// let res = user.sort((a, b) => { // sonlarni o'sish tartida joylashtiradi ,Alfabit tartibida joylashtidi
+//     return a.name.localeCompare(b.name)
+// })
+// console.log(res);
+// let res = str.find((valyus) => { //Ichida malumot bor yoki yo'qligini teshirib beradi
+//         // return valyus.id === 5
+//         console.log(str[5]);
+//     })
+//     // console.log(res);
 let user = [
-    { id: 1, year: 1998, engine: 1, name: "Tico" },
-    { id: 2, year: 2005, engine: 1.2, name: "Matiz" },
-    { id: 3, year: 2010, engine: 1.5, name: "Gentra" },
-    { id: 4, year: 2010, engine: 1.2, name: "Cobalt" },
-    { id: 5, year: 2012, engine: 2, name: "Malibu" },
-    { id: 6, year: 2000, engine: 1.2, name: "Damaz" },
-    { id: 7, year: 2018, engine: 2.4, name: "Tracer" },
-]
-const getbayid = () => { ////   2000 chi yildan oldin chiqqan mashinalar ro'yxait
-        let res = user.filter((v) => {
-            return v.year < 2000
-        })
-        user = res
-        console.log(res);
-    }
-    // getbayid()
-const getbay = () => {
-        let res1 = user.filter((v) => {
-            return v.year > 2010
-        })
-        user = res1
-        console.log(res1);
-    }
-    // getbay()
-const getbays = () => {
-        let res2 = user.sort((a, b) => {
-            return a.engine - b.engine
-        })
-        user = res2
-        console.log(res2);
-    }
-    // getbays()
-const getbayd = () => {
-        let res4 = user.sort((a, b) => {
-            return a.year - b.year
-        })
-        user = res4
-        console.log(res4);
-    }
-    // getbayd()
-const getbayf = () => {
-        let res5 = user.sort((a, b) => {
-            return a.name.localeCompare(b.name)
-        })
-        user = res5
-        console.log(res5);
-    }
-    // getbayf()
-const getbayname = () => {
-    let res7 = user.map((v) => {
-        return v.year < 2000 ? {...user, status: "o'rta" } : v
-    })
-    user = res7
-    console.log(res7);
-}
-getbayname()
+        { id: 1, year: 1998, engine: 1, name: "Tico" },
+        { id: 2, year: 2005, engine: 1.2, name: "Matiz" },
+        { id: 3, year: 2010, engine: 1.5, name: "Gentra" },
+        { id: 4, year: 2010, engine: 1.2, name: "Cobalt" },
+        { id: 5, year: 2012, engine: 2, name: "Malibu" },
+        { id: 6, year: 2000, engine: 1.2, name: "Damaz" },
+        { id: 7, year: 2018, engine: 2.4, name: "Tracer" },
+    ]
+    // let res = str.forEach((v) => {
+    //     console.log(v);
+    // })
+    // let resmap = str.map((v) => {
+
+//     return v + 2
+// })
+// console.log(resmap);
+// let str = [1, "web", ]
+// console.log(str.some((v) => {
+//     return !isNaN(v)
+// }));
+// console.log(str.every((v) => {
+//     return !isNaN(v)
+// }));
+// let res = str.fill(22, 4, 5)
+// console.log(res);
+// let res = str.copyWithin(10, 3, 6)
+// console.log(res);
+// let res = str.reduce((valyus, boshlangich) => {
+//     return valyus + boshlangich
+// }, 0)
+// console.log(res);
+// let res = str.flat(2)
+// console.log(res);
+// let res = str.flatMap((v) => {
+//     return v
+// })
+// console.log(res);
+// let str = [1, 2, 2, 3, 3, 4, 4, 5, 6, 7, 8]
+// let res = str.filter((valyus, index, arr) => {
+//     return arr.indexOf(valyus) === index
+// })
+// console.log(res);
