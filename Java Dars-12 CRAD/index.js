d301c7523242349511520910b9910e94fc6bc2c2
Muhammadjon.2005 <Muhammadjonmurodullayev2005@gmail.com>
2023-08-12 16:39:26 +0500

    Dars-12

---
+++ Java Dars-12 CRAD/index.js
@@ -0,0 +1,65 @@
+// let str = "web"
+// let str2 = "bew"
+// let res = str.split("")
+// let res1 = str2.split("")
+// const sum = res.sort((a, b) => {
+//     return str = a.localeCompare(b)
+// })
+// const sum1 = res1.sort((a, b) => {
+//     return a.localeCompare(b)
+// })
+// let sum2 = str = sum.join()
+// let sum3 = str2 = sum.join()
+// console.log(str == str2);
+// ====================================================
+// Palindirom =so'zlar
+// let res = "kiyik"
+// let res1 = res.split("")
+// let res3 = res1.reverse()
+// let res4 = res3.join()
+//     // if (res == res4) {
+// console.log(res == res4);
+// // }
+// console.log(res4);
+// const getname = (data) => {
+//     let res = user.map((v) => v.id === data.id ? {...v, [data.type]: data.valyus } : v)
+//     user = res
+// }
+// getname({ id: 4, type: "year", valyus: 2024 })
+// getname({ id: 4, type: "engine", valyus: 6 })
+// getname({ id: 4, type: "name", valyus: `BMW` })
+// console.log(user);
+// const getname = (data) => {
+//     let res = user.map((v) => v.id === data.id ? {...v, [data.valyu]: data.keys } : v)
+//     user = res
+// }
+// getname({ id: 4, valyu: "name", keys: "Muhammadjon" })
+// console.log(user);
+// let user = [
+//         { id: 1, year: 1998, engine: 1, name: "CEO" },
+//         { id: 2, year: 2005, engine: 1.2, name: "CEO" },
+//         { id: 3, year: 2010, engine: 1.5, name: "CEO" },
+//         { id: 4, year: 2023, engine: 4.4, name: "BMW" },
+//         { id: 5, year: 2012, engine: 2, name: "CTO" },
+//         { id: 6, year: 2000, engine: 1.2, name: "CTO" },
+//         { id: 7, year: 2018, engine: 2.4, name: "CTO" },
+//     ]
+// const getname = () => {
+//     let res1 = user.filter((v) => {
+//         if (v.id == "CEO") {
+//             res.ceo.values.push()
+//         }
+//     })
+// }
+// let res = {
+//     ceo: [],
+//     cto: []
+// }
+// console.log(res);
+let res = "234567892"
+    // let res1 = 0
+for (i of res) {
+    console.log(i * i);
+    // console.log(i);
+}
+// console.log(res1);
