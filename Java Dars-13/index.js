2e8ff3cedf6bcb35df10410c0acd511908f3890e
Muhammadjon.2005 <Muhammadjonmurodullayev2005@gmail.com>
2023-08-19 16:01:55 +0500

    Dars-13

--- Java Dars-13/index.js
+++ Java Dars-13/index.js
@@ -34,44 +34,56 @@
 // console.log(data);let data = new Date()
 let hafta = [`Yakshanba`, `Dushanba`, `Seshanba`, `Chorshanba`, `Payshanba`, `Juma`, `Shanba`]
 const getname = () => {
+        return {
+            parametr(date) {
+                switch (date) {
+                    case `MMMM do YYYY,h:mm:ss a`:
+                        return `${hafta[new Date().getDay()]} ${new Date().getDate()} ${new Date().getFullYear()},${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()} pm`
+                    case `dddd`:
+                        return hafta[new Date().getDay()]
+                }
+            }
+        }
+    }
+    // console.log(getname().parametr("dddd"));`
+let arr = ["Dushanba", "Seshanba", "Chorshanba", "Payshanba", "Juma", "Shanba", "Yakshanba"]
+let arr1 = ["Yanvar", "Fevral", "Mart", "Aprel", "May", "Iyun", "Iyul", "Avgust", "Sentyabr", "Oktyabr", "Noyabr", "Dekabr"]
+    // const get = (data) => {
+    //     let time = new Date()
+    //     if (data == "") {
+    //         console.log("en");
+    //     } else if (data == "LTS") {
+    //         console.log(`${time.getHours() % 12}:${time.getMinutes()}:${time.getSeconds()} PM`);
+    //     } else if (data == "LT") {
+    //         console.log(`Today: ${time.getHours() % 12}:${time.getMinutes()} PM`);
+    //     } else if (data == "L") {
+    //         console.log(time.getMonth() < 10 ? `0${time.getMonth()}/${time.getDate()}/${time.getFullYear()}` : `${time.getMonth()}/${time.getDate()}/${time.getFullYear()}`);
+    //     } else if (data == "l") {
+    //         console.log(`${time.getMonth()}/${ti - me.getDate()}/${time.getFullYear()}`);
+    //     } else if (data == "LL") {
+    //         console.log(`${arr1[time.getMonth()]} ${time.getDate()},${time.getFullYear()}`);
+    //     } else if (data == "ll") {
+    //         console.log(`${arr1[time.getMonth()].slice(0, 3)} ${time.getDate()},${time.getFullYear()}`);
+    //     } else if (data == "LLL") {
+    //         console.log(`${arr1[time.getMonth()]} ${time.getDate()}, ${time.getFullYear()} ${time.getHours() % 12}:${time.getSeconds()} PM`);
+    //     } else if (data == "lll") {
+    //         console.log(`${arr1[time.getMonth()].slice(0, 3)} ${time.getDate()}, ${time.getFullYear()} ${time.getHours() % 12}:${time.getSeconds()} PM`);
+    //     } else if (data == "LLLL") {
+    //         console.log(`${arr[time.getDay()]}, ${arr1[time.getMonth()]}, ${time.getFullYear()} ${time.getHours() % 12}:${time.getSeconds()} PM`);
+    //     } else if (data == "llll") {
+    //         console.log(`${arr[time.getDay()].slice(0, 3)}, ${arr1[time.getMonth()].slice(0, 3)}, ${time.getFullYear()} ${time.getHours() % 12}:${time.getSeconds()} PM`);
+    //     }
+    // }
+    // get("lll")
+const getnam = () => {
+    var data = new Date()
     return {
-        parametr(date) {
-            switch (date) {
-                case `MMMM do YYYY,h:mm:ss a`:
-                    return `${hafta[new Date().getDay()]} ${new Date().getDate()} ${new Date().getFullYear()},${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()} pm`
-                case `dddd`:
-                    return hafta[new Date().getDay()]
+        getsurname(kun) {
+            switch (kun) {
+                case `lll`:
+                    return `Soat ${data.getHours()}:${data.getMinutes()}:${data.getSeconds()}`
             }
         }
     }
 }
-console.log(getname().parametr("dddd"));
-let arr = ["Dushanba", "Seshanba", "Chorshanba", "Payshanba", "Juma", "Shanba", "Yakshanba"]
-let arr1 = ["Yanvar", "Fevral", "Mart", "Aprel", "May", "Iyun", "Iyul", "Avgust", "Sentyabr", "Oktyabr", "Noyabr", "Dekabr"]
-const get = (data) => {
-    let time = new Date()
-    if (data == "") {
-        console.log("en");
-    } else if (data == "LTS") {
-        console.log(`${time.getHours() % 12}:${time.getMinutes()}:${time.getSeconds()} PM`);
-    } else if (data == "LT") {
-        console.log(`Today: ${time.getHours() % 12}:${time.getMinutes()} PM`);
-    } else if (data == "L") {
-        console.log(time.getMonth() < 10 ? `0${time.getMonth()}/${time.getDate()}/${time.getFullYear()}` : `${time.getMonth()}/${time.getDate()}/${time.getFullYear()}`);
-    } else if (data == "l") {
-        console.log(`${time.getMonth()}/${ti - me.getDate()}/${time.getFullYear()}`);
-    } else if (data == "LL") {
-        console.log(`${arr1[time.getMonth()]} ${time.getDate()},${time.getFullYear()}`);
-    } else if (data == "ll") {
-        console.log(`${arr1[time.getMonth()].slice(0, 3)} ${time.getDate()},${time.getFullYear()}`);
-    } else if (data == "LLL") {
-        console.log(`${arr1[time.getMonth()]} ${time.getDate()}, ${time.getFullYear()} ${time.getHours() % 12}:${time.getSeconds()} PM`);
-    } else if (data == "lll") {
-        console.log(`${arr1[time.getMonth()].slice(0, 3)} ${time.getDate()}, ${time.getFullYear()} ${time.getHours() % 12}:${time.getSeconds()} PM`);
-    } else if (data == "LLLL") {
-        console.log(`${arr[time.getDay()]}, ${arr1[time.getMonth()]}, ${time.getFullYear()} ${time.getHours() % 12}:${time.getSeconds()} PM`);
-    } else if (data == "llll") {
-        console.log(`${arr[time.getDay()].slice(0, 3)}, ${arr1[time.getMonth()].slice(0, 3)}, ${time.getFullYear()} ${time.getHours() % 12}:${time.getSeconds()} PM`);
-    }
-}
-get("LLLL")
+console.log(getnam().getsurname("lll"));
