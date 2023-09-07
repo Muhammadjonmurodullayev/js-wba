f5ff84d3b6a3319f026160fd1ce3a2de6de270d7
Muhammadjon.2005 <Muhammadjonmurodullayev2005@gmail.com>
2023-09-07 14:00:18 +0500

    Dars-16

--- JAva Dars-16/index.js
+++ JAva Dars-16/index.js
@@ -1,15 +1,40 @@
-class User {
-    constructor(thisfronted, thispassword) {
-        this.Login = thisfronted
-        this.password = thispassword
-    }
-    Login = "Muhammadjon"
-    password = 12345
-    age = 18
-    getname() {
-        console.log(`Foydalanuvchi logini:${this.Login} Paroli:${this.password} Yoshi:${this.age}`);
-    }
-}
-let user1 = new User("fronted", 7726700)
-console.log(user1);
-user1.getname()
+// class User {
+//     constructor(thisfronted, thispassword) {
+//         this.Login = thisfronted
+//         this.password = thispassword
+//     }
+//     Login = "Muhammadjon"
+//     password = 12345
+//     age = 18
+//     getname() {
+//         console.log(`Foydalanuvchi logini:${this.Login} Paroli:${this.password} Yoshi:${this.age}`);
+//     }
+// }
+// // let user1 = new User("fronted", 7726700)
+// class Get extends User {}
+// let user2 = new Get("web", 2005)
+// console.log(user2);
+// class user {
+//     name = "web"
+//     age = 18
+//     static name = "Muhammadjon"
+// }
+// let res = new user()
+// console.log(res);
+// console.log(user);
+// class user extends Array {
+
+// }
+// let res = new user()
+// res.push("Muhammadjon")
+// res.push("Ozodbek")
+// console.log(res);
+// try {
+//     console.log(a);
+
+// } catch (error) {
+//     console.log(error.message);
+//     console.log(error.name);
+//     // console.log(error.stack);
+// }
+// console.log("Muhammadjon");
